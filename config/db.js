dec11f4a25b704c106a80619bebf875b78aad82a
amarchauhan9758 <aryanchauhan9758@gmail.com>
2025-08-17 16:53:33 +0530

    code commit regardingremovoe   secret kry from db.js

--- config/db.js
+++ config/db.js
@@ -3,14 +3,6 @@ require("dotenv").config();
 
 const connectDB = async () => {
   mongoose.connect(process.env.MONGO_URI);
-  // mongoose.connect('mongodb+srv://amarchauhan06232:HnrpnED16vULbwHe@cluster0.qul64bv.mongodb.net/devTinder')
 };
 
 module.exports = connectDB;
-
-// connectDB()
-//     .then(console.log(
-//         'database connection established'
-//     ))
-//     .catch((e) => console.log(e))
-// .finally(() => mongoose.close())
